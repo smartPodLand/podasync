84857ad2b94b83a438ee78770e69639432c57ae1
masoudmanson <masoudmanson@gmail.com>
2018-02-21 16:32:13 +0330

    Add Comments

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Make Chatting Possible
+ * @param {json object} options
+ * @return {boolean}
+ */
+
 module.exports = (options) => {
 	console.log(options);
 	return true;	
